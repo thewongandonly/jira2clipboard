039f9153d2e2ec6e2102d1fa18eaffdef2a4b01d
David Wong <1740957+thewongandonly@users.noreply.github.com>
2024-10-19 11:51:55 +0200

    manifest

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# jira2clipboard
-copy jira story to clipboard as git comment
+# Jira Story to clipboard
+
+When committing to git, simply use this chrome extension to copy current jira headline to clipboard.
+
+This sample demonstrates how to use the [chrome.action](https://developer.chrome.com/docs/extensions/reference/api/action) API to execute code when the extension icon is clicked.
+
+## Overview
+
+This extension collects jira story on the active tab page for easy git commits.
+PS: tab must have focus
+
+## Running this extension
+
+1. Clone this repository.
+2. Load this directory in Chrome as an [unpacked extension](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
+3. Navigate to (https://jira.vijzelmolen.nl/) page and click on either a board or story detail.
+4. Click the extension icon.
+
+## Firefox
+
+Note: requires a developer edition, nightly or unbranded build of Firefox! Release build require a signed extension or only allows loading as a temporary extension (lost after restart).
+
+1. Clone this repository
+2. zip folder and rename zip extension to xpi
+3. Open Firefox, go to about:config and change xpinstall.signatures.required to false
+4. Go to Add-on and themes, click the cog and Install add-on from file...
---
+++ service-worker.js
@@ -0,0 +1,160 @@
+function collectStory() {
+  let resolvedVal = "";
+
+  const sendMessage = async (textToCopy) => {
+    const result = await chrome.runtime.sendMessage({ clipped: textToCopy });
+    //console.log(result);
+  };
+
+  const sendError = async (error) => {
+    const result = await chrome.runtime.sendMessage({ error });
+    //console.log(result);
+  };
+
+  const copyClip = function (textToCopy) {
+    if (!textToCopy) return;
+    const clipBoard = navigator.clipboard;
+    clipBoard.writeText(textToCopy).then(() => {
+      console.log(textToCopy);
+
+      //send result back to worker:
+      sendMessage(textToCopy);
+    });
+  };
+
+  let subTasks = document.body.querySelector(
+    ".ghx-parent-group div.ghx-selected"
+  );
+  if (!!subTasks) {
+    const taskcontainer = document.body.querySelector(
+      ".ghx-parent-group :has(> .ghx-selected) "
+    );
+
+    if (!!taskcontainer) {
+      const parent = taskcontainer.parentElement;
+
+      if (!!parent) {
+        const keyresolved = parent.getAttribute("data-issue-key");
+        // console.log(keyresolved);
+        const querySelectorkey =
+          ".ghx-parent-group[data-issue-key='" +
+          keyresolved +
+          "'] .ghx-parent-stub";
+        // console.log(querySelectorkey);
+        const titeldiv = document.body.querySelector(querySelectorkey);
+        resolvedVal = titeldiv.getAttribute("title");
+
+        // console.log(resolvedVal);
+      }
+    }
+  }
+
+  let tegel = document.body.querySelector("div.ghx-selected");
+  if (!!tegel) {
+    //hoofdstorytegel:
+    resolvedVal = !!resolvedVal ? resolvedVal + "\r\n- " : "";
+    resolvedVal = resolvedVal + tegel.getAttribute("aria-label");
+
+    // console.log(resolvedVal);
+  }
+
+  if (!!resolvedVal) {
+    copyClip(resolvedVal);
+    return;
+  }
+
+  //detail view:
+  const myselector = "issue-view";
+  let detailPage = document.querySelector(`.${CSS.escape(myselector)}`);
+  if (!!detailPage) {
+    const tkey = "aui-page-header-main";
+    let detailPagemain = document.querySelector(`.${CSS.escape(tkey)}`);
+    if (!!detailPagemain) {
+      const anchorHeaderTagsParent = document.body.querySelector(
+        ".aui-page-header-main a#parent_issue_summary.issue-link"
+      );
+      const anchorHeaderTags = document.body.querySelector(
+        ".aui-page-header-main a#key-val.issue-link"
+      );
+      let storynummer = anchorHeaderTags.getAttribute("data-issue-key");
+      const summaryElement = document.querySelector(
+        ".aui-page-header-main h1#summary-val"
+      );
+      if (!!anchorHeaderTagsParent) {
+        let storynummerParent = anchorHeaderTagsParent.textContent;
+        resolvedVal =
+          storynummerParent +
+          "\r\n- " +
+          storynummer +
+          ": " +
+          summaryElement.textContent;
+      } else {
+        //detail pagina zonder parent:
+        resolvedVal = storynummer + ": " + summaryElement.textContent;
+      }
+
+      if (!!resolvedVal) {
+        copyClip(resolvedVal);
+        return;
+      }
+    }
+  }
+
+  sendError('jira story not found here');
+}
+
+
+function copyStory(tab) {
+  if (tab.url.startsWith("https:")) {//add you own rule
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: collectStory,
+    });
+  }
+   
+}
+
+
+function notify(message) {
+  chrome.storage.sync.get({ skipNotify: false }).then((items) => {
+    if (!items.skipNotify) {
+      doNotify(message);
+    }
+  });
+}
+
+
+function notifyError(error) {
+  doNotify('Fout: ' + error);
+}
+
+
+function doNotify(message) {
+  chrome.notifications.create("", {
+    type: "basic",
+    title: "Copy story",
+    message: message,
+    iconUrl: "cake_128x128.png",
+  });
+}
+
+chrome.action.onClicked.addListener(copyStory);
+
+ 
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1843866
+chrome.commands.onCommand.addListener((command) => {
+  chrome.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
+    if (command === "copy-story") 
+      copyStory(tabs[0]);
+  });
+});
+
+chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
+  if (!!data.clipped) {
+    notify(data.clipped);
+  }
+
+  if (!!data.error) {
+    notifyError(data.error);
+  }
+});
